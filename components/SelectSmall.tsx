57e9787df6529970ee62caa72d2b21f795a77637
Aluno <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>
2022-08-01 14:07:51 -0300

    chore: updating select component

---
+++ components/SelectSmall.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
+const currencies = [
+  {
+    value: 'portugues',
+    label: 'Português',
+  },
+  {
+    value: 'matematica',
+    label: 'Matemática',
+  },
+  {
+    value: 'historia',
+    label: 'História',
+  },
+  {
+    value: 'biologia',
+    label: 'Biologia',
+  },
+];
+
+export default function SelectTextFields() {
+  const [currency, setCurrency] = React.useState('EUR');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrency(event.target.value);
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          id="outlined-select-currency"
+          select
+          label="Disciplina"
+          value={currency}
+          onChange={handleChange}
+          size="small"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+      </div>
+    </Box>
+  );
+}
