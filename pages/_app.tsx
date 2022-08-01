0a6322221b7e74bf4e6e529518f1d7d38500c449
Jessé Diego <64742095+JezzDiego@users.noreply.github.com>
2022-08-01 13:19:43 -0300

    Revert "Questions"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
-import "rbx/index.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
-      <Navbar />
+      <Navbar></Navbar>
       <Component {...pageProps} />
     </div>
   );
