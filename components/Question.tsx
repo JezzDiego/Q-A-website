bb2e5e37ff9909682a65bf145bf334473fee8c0e
JezzDiego <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>
2022-08-01 22:23:54 -0300

    chore: changing the method of fetch Questions data

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,56 +1,88 @@
-import * as React from 'react';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormLabel from '@mui/material/FormLabel';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormLabel from "@mui/material/FormLabel";
+import Button from "@mui/material/Button";
 
-export default function Question() {
-  const [value, setValue] = React.useState('');
+interface QuestionProps {
+  questions: {
+    id: number;
+    question: string;
+    alt1: {
+      name: string;
+      slug: string;
+    };
+    alt2: {
+      name: string;
+      slug: string;
+    };
+    correct: string;
+  }[];
+}
+
+export default function Question(props: QuestionProps) {
+  const [value, setValue] = React.useState("");
   const [error, setError] = React.useState(false);
-  const [helperText, setHelperText] = React.useState('Choose wisely');
+  const [helperText, setHelperText] = React.useState("Escolha uma opção");
 
   const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue((event.target as HTMLInputElement).value);
-    setHelperText(' ');
+    setHelperText(" ");
     setError(false);
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (value === 'alt2') {
-      setHelperText('You got it!');
+    if (value === "joao") {
+      setHelperText("You got it!");
       setError(false);
-    } else if (value === 'alt1') {
-      setHelperText('Sorry, wrong answer!');
+    } else if (value === "maria") {
+      setHelperText("Sorry, wrong answer!");
       setError(true);
     } else {
-      setHelperText('Please select an option.');
+      setHelperText("Please select an option.");
       setError(true);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FormControl sx={{ m: 3 }} error={error} variant="standard">
-        <FormLabel id="demo-error-radios">Pergunta?</FormLabel>
-        <RadioGroup
-          aria-labelledby="demo-error-radios"
-          name="quiz"
-          value={value}
-          onChange={handleRadioChange}
-        >
-          <FormControlLabel value="alt1" control={<Radio />} label="Alternativa 1" />
-          <FormControlLabel value="alt2" control={<Radio />} label="Alternativa 2" />
-        </RadioGroup>
-        <FormHelperText>{helperText}</FormHelperText>
-        <Button sx={{ mt: 1, mr: 1 }} type="submit" variant="outlined">
-          Check Answer
-        </Button>
-      </FormControl>
-    </form>
+    <div>
+      {props.questions.map((question) => (
+        <form onSubmit={handleSubmit} tabIndex={question.id}>
+          <FormControl sx={{ m: 3 }} error={error} variant="standard">
+            <FormLabel id="demo-error-radios">{question.question}</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-error-radios"
+              name="quiz"
+              value={value}
+              onChange={handleRadioChange}
+            >
+              <FormControlLabel
+                value={question.alt1.slug}
+                control={<Radio />}
+                label={question.alt1.name}
+              />
+              <FormControlLabel
+                value={question.alt2.slug}
+                control={<Radio />}
+                label={question.alt2.name}
+              />
+            </RadioGroup>
+            <FormHelperText>{helperText}</FormHelperText>
+            <Button
+              sx={{ mt: 1, mr: 1, maxWidth: 110 }}
+              type="submit"
+              variant="contained"
+            >
+              Responder
+            </Button>
+          </FormControl>
+        </form>
+      ))}
+    </div>
   );
 }
--- pages/questoes/index.tsx
+++ pages/questoes/index.tsx
@@ -2,18 +2,60 @@ import Question from "../../components/Question";
 import Search from "../../components/Search";
 import index from "../../styles/index.module.scss";
 
-export default function Questions() {
-    return (
-        <>
-            <div>
-                <header className={index.header}>
-                    <Search />
-                </header>
+const questions = [
+  {
+    id: 0,
+    question: "Qual o seu nome?",
+    alt1: {
+      name: "João",
+      slug: "joao",
+    },
+    alt2: {
+      name: "Maria",
+      slug: "maria",
+    },
+    correct: "joao",
+  },
+  {
+    id: 1,
+    question: "Qual o seu sobrenome?",
+    alt1: {
+      name: "Silva",
+      slug: "silva",
+    },
+    alt2: {
+      name: "Santos",
+      slug: "santos",
+    },
+    correct: "santos",
+  },
+  {
+    id: 2,
+    question: "Qual o seu nome de usuário?",
+    alt1: {
+      name: "Joaozinho",
+      slug: "joaozinho",
+    },
+    alt2: {
+      name: "Mariazinha",
+      slug: "mariazinha",
+    },
+    correct: "joaozinho",
+  },
+];
 
-                <main>
-                    <Question />
-                </main>
-            </div>
-        </>
-    )
+export default function Questions() {
+  return (
+    <>
+      <div>
+        <header className={index.header}>
+          <Search />
+        </header>
+
+        <main>
+          <Question questions={questions} />
+        </main>
+      </div>
+    </>
+  );
 }
