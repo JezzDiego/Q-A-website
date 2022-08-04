adcddab203704eb4ca5345e2381af122be7b2d55
Jessé Diego <jdso1@aluno.ifal.edu.br>
2022-08-04 19:53:42 +0000

    chore: redering all news from a array of object

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,32 @@ import type { NextPage } from "next";
 import NewsCard from "../components/NewsCard";
 import index from "../styles/index.module.scss";
 
+const News = [
+  {
+    id: 0,
+    time: "08/08/2022",
+    content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum quisquam, aliquam totam eius quod illo fuga cupiditate expedita? Alias, repudiandae. Explicabo enim neque nulla obcaecati reiciendis accusamus sunt nihil repudiandae!"
+  },
+  {
+    id: 1,
+    time: "08/08/2022",
+    content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum quisquam, aliquam totam eius quod illo fuga cupiditate expedita? Alias, repudiandae. Explicabo enim neque nulla obcaecati reiciendis accusamus sunt nihil repudiandae!"
+  } 
+  ]
+
 const Home: NextPage = () => {
   return (
-    <>
-      <div className={index.centerChild}>
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-        <NewsCard />
-      </div>
-    </>
-  );
+        <>
+          <div className={index.centerChild}>
+            {
+              News.map((news) => {
+                return (
+                  <NewsCard key={news.id} news={news} />
+                )
+              })
+            }
+          </div>
+        </>
+      );
 };
-
 export default Home;
