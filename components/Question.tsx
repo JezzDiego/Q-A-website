5cfcbc42995ef87476b620b1ae9780dc34625870
JezzDiego <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>
2022-08-07 15:36:36 -0300

    chore: making each question to have five alternatives

--- components/Question.tsx
+++ components/Question.tsx
@@ -8,19 +8,31 @@ import FormLabel from "@mui/material/FormLabel";
 import Button from "@mui/material/Button";
 
 interface QuestionProps {
-    question: {
-      id: number;
-      request: string;
-      alt1: {
-        name: string,
-        slug: string,
-      },
-      alt2: {
-        name: string,
-        slug: string,
-      },
-      correct: string;
-    }
+  question: {
+    id: number;
+    request: string;
+    alt1: {
+      name: string;
+      slug: string;
+    };
+    alt2: {
+      name: string;
+      slug: string;
+    };
+    alt3: {
+      name: string;
+      slug: string;
+    };
+    alt4: {
+      name: string;
+      slug: string;
+    };
+    alt5: {
+      name: string;
+      slug: string;
+    };
+    correct: string;
+  };
 }
 
 export default function Question(props: QuestionProps) {
@@ -48,37 +60,57 @@ export default function Question(props: QuestionProps) {
 
   return (
     <div>
-        <form onSubmit={handleSubmit} key={props.question.id}>
-          <FormControl sx={{ m: 3 }} error={error} variant="standard">
-            <FormLabel id="demo-error-radios" sx={{fontSize: 25}}>{props.question.request}</FormLabel>
-            <RadioGroup
-              aria-labelledby="demo-error-radios"
-              name="quiz"
-              value={value}
-              onChange={handleRadioChange}
-            >
-              <FormControlLabel
-                value={props.question.alt1.slug}
-                control={<Radio />}
-                label={props.question.alt1.name}
-              />
-              <FormControlLabel
-                value={props.question.alt2.slug}
-                control={<Radio />}
-                label={props.question.alt2.name}
-              />
-            </RadioGroup>
-            <FormHelperText sx={{fontSize: 18}}>{helperText}</FormHelperText>
-            <Button
-              sx={{ mt: 1, mr: 1, maxWidth: 110 }}
-              type="submit"
-              variant="contained"
-              color="warning"
-            >
-              Responder
-            </Button>
-          </FormControl>
-        </form>
+      <form onSubmit={handleSubmit} key={props.question.id}>
+        <FormControl sx={{ m: 3 }} error={error} variant="standard">
+          <FormLabel
+            id="demo-error-radios"
+            sx={{ fontSize: 20, textAlign: "justify" }}
+          >
+            {props.question.request}
+          </FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-error-radios"
+            name="quiz"
+            value={value}
+            onChange={handleRadioChange}
+          >
+            <FormControlLabel
+              value={props.question.alt1.slug}
+              control={<Radio />}
+              label={props.question.alt1.name}
+            />
+            <FormControlLabel
+              value={props.question.alt2.slug}
+              control={<Radio />}
+              label={props.question.alt2.name}
+            />
+            <FormControlLabel
+              value={props.question.alt3.slug}
+              control={<Radio />}
+              label={props.question.alt3.name}
+            />
+            <FormControlLabel
+              value={props.question.alt4.slug}
+              control={<Radio />}
+              label={props.question.alt4.name}
+            />
+            <FormControlLabel
+              value={props.question.alt5.slug}
+              control={<Radio />}
+              label={props.question.alt5.name}
+            />
+          </RadioGroup>
+          <FormHelperText sx={{ fontSize: 18 }}>{helperText}</FormHelperText>
+          <Button
+            sx={{ mt: 1, mr: 1, maxWidth: 110 }}
+            type="submit"
+            variant="contained"
+            color="warning"
+          >
+            Responder
+          </Button>
+        </FormControl>
+      </form>
     </div>
   );
 }
--- pages/questoes/index.tsx
+++ pages/questoes/index.tsx
@@ -5,42 +5,81 @@ import index from "../../styles/index.module.scss";
 const questions = [
   {
     id: 0,
-    request: "Guilherme, por acaso o nome do seu pai seria...",
+    request:
+      "Seis jogadores, vestidos com camisas numeradas de 1 até 6, formaram uma fila. O técnico fez um aquecimento da seguinte maneira: Ele primeiro gritou “três” e, por 3 vezes, quem estava no começo da fila correu até o fim da fila, ou seja, considerando a ordem da fila antes do grito, o primeiro foi para o fim da fila, depois o segundo foi para o fim da fila e finalmente o terceiro foi para o fim da fila; nesse momento, o técnico observou que quem estava no começo da fila era o jogador com a camisa 3 e ele pediu que esse jogador saísse da fila. Em seguida, ele gritou “seis” e, por 6 vezes, quem estava no começo da fila correu até o fim da fila, de maneira que houve jogador que voltou para o fim da fila mais de uma vez. Quando o processo se repetiu as 6 vezes, o técnico observou que quem estava no começo da fila era o jogador com a camisa 6 e ele pediu que esse jogador saísse da fila. O técnico repetiu esse processo todo mais três vezes, tendo gritado “cinco”, depois, “quatro” e depois “dois”, e ao fim de cada comando percebeu que estavam no começo da fila, respectivamente, os jogadores com as camisas 5, 4 e 2 e pediu, a cada vez, que o respectivo jogador saísse da fila. Quando sobrou somente o jogador com a camisa número 1 na fila, o técnico perguntou: “você estava atrás de quem na fila original?”, então esse jogador respondeu corretamente que estava atrás do jogador de número",
     alt1: {
-      name: "João",
-      slug: "joao",
+      name: "3",
+      slug: "A",
     },
     alt2: {
-      name: "Maria",
-      slug: "maria",
+      name: "4",
+      slug: "B",
     },
-    correct: "joao",
+    alt3: {
+      name: "6",
+      slug: "C",
+    },
+    alt4: {
+      name: "5",
+      slug: "D",
+    },
+    alt5: {
+      name: "2",
+      slug: "E",
+    },
+    correct: "D",
   },
   {
     id: 1,
-    request: "Qual o seu sobrenome?",
+    request:
+      "Uma fazenda tem 14 funcionários e todos sabem cuidar de cavalos ou bois ou cachorros. Apenas um funcionário sabe cuidar de qualquer uma dessas espécies e quem sabe cuidar de boi, também sabe cuidar de cavalo. O número de funcionários que cuidam apenas de cavalos é igual ao número dos que cuidam apenas de cavalos e bois. O funcionário que sabe cuidar de ambos, cavalos e cachorros, também sabe cuidar de bois e 5 funcionários só sabem cuidar de cachorros. O número de funcionários que sabe cuidar apenas de cavalos é",
     alt1: {
-      name: "Silva",
-      slug: "silva",
+      name: "3",
+      slug: "A",
     },
     alt2: {
-      name: "Santos",
-      slug: "santos",
+      name: "6",
+      slug: "B",
     },
-    correct: "santos",
+    alt3: {
+      name: "5",
+      slug: "C",
+    },
+    alt4: {
+      name: "7",
+      slug: "D",
+    },
+    alt5: {
+      name: "4",
+      slug: "E",
+    },
+    correct: "E",
   },
   {
     id: 2,
-    request: "Qual o seu nome de usuário?",
+    request:
+      "Ao visitar a tradicional ilha, onde parte dos habitantes só falam verdades e os demais habitantes só falam mentiras, um turista encontrou três habitantes, um vestido de verde, outro de vermelho e outro de azul. O turista perguntou ao habitante de azul qual a cor da própria camisa, em seguida pediu para o habitante de vermelho fazer uma afirmação e finalmente pediu ao que estava vestido de verde para repetir o que havia respondido o habitante de azul. O turista obteve, respectivamente, as seguintes respostas: “verde”, “quem está de verde está mentindo”, “verde”.Os habitantes vestidos de verde, vermelho e azul falam, respectivamente,",
     alt1: {
-      name: "Joaozinho",
-      slug: "joaozinho",
+      name: "verdades, mentiras, verdades.",
+      slug: "A",
     },
     alt2: {
-      name: "Mariazinha",
-      slug: "mariazinha",
+      name: "mentiras, mentiras, verdades.",
+      slug: "B",
     },
-    correct: "joaozinho",
+    alt3: {
+      name: "mentiras, verdades, mentiras.",
+      slug: "C",
+    },
+    alt4: {
+      name: "verdades, verdades, mentiras.",
+      slug: "D",
+    },
+    alt5: {
+      name: "verdades, mentiras, mentiras.",
+      slug: "E",
+    },
+    correct: "E",
   },
 ];
 
@@ -53,14 +92,9 @@ export default function Questions() {
         </header>
 
         <main>
-          
-          {
-            questions.map((question) => {
-              return (
-                <Question key={question.id} question={question} />
-              )
-            })
-          }
+          {questions.map((question) => {
+            return <Question key={question.id} question={question} />;
+          })}
         </main>
       </div>
     </>
