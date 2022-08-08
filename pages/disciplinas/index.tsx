3cfef9ec9169c3a2deffc5eba8c16f441f2f48c0
Jessé Diego <jdso1@aluno.ifal.edu.br>
2022-08-08 12:37:16 +0000

    chore: changing some style properties

--- pages/disciplinas/index.tsx
+++ pages/disciplinas/index.tsx
@@ -1,7 +1,18 @@
-export default function index() {
-    return (
-        <div>
-            <h1>OI</h1>
+import BasicTextFields from "../../components/TextField";
+import index from '../../styles/index.module.scss'
+import { BsSearch } from "react-icons/bs";
+
+export default function Subjects() {
+  return (
+    <>
+      <div>
+        <div className={index.centerSearch}>
+          <BasicTextFields />
+          <button className={index.centeredIcon}>
+            <BsSearch size={20}></BsSearch>
+          </button>
         </div>
-    )
+      </div>
+    </>
+  );
 }
