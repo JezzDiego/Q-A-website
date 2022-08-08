ba8208534627ba2e18edbaf69f5b53850690a2ef
Jessé Diego <jdso1@aluno.ifal.edu.br>
2022-08-08 12:46:30 +0000

    transformin subjects array into a json

--- components/SelectSmall.tsx
+++ components/SelectSmall.tsx
@@ -2,25 +2,7 @@ import { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
-
-const subjects = [
-  {
-    value: "portugues",
-    label: "Português",
-  },
-  {
-    value: "matematica",
-    label: "Matemática",
-  },
-  {
-    value: "historia",
-    label: "História",
-  },
-  {
-    value: "biologia",
-    label: "Biologia",
-  },
-];
+import materias from '../pages/api/subjects.json'
 
 export default function SelectTextFields() {
   const [currency, setCurrency] = useState("");
@@ -47,7 +29,7 @@ export default function SelectTextFields() {
           onChange={handleChange}
           size="small"
         >
-          {subjects.map((option) => (
+          {materias.subjects.map((option) => (
             <MenuItem key={option.value} value={option.value}>
               {option.label}
             </MenuItem>
